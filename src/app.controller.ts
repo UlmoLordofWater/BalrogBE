853425f2a300563ebeb8cb10e3de9325424a2a1f
UlmoLordofWater <wesley.duris@gmail.com>
2023-10-30 17:32:30 -0400

    passport work, adding jwt

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,84 +1,106 @@
 import {
   Controller,
+  Request,
   Get,
   Param,
   Post,
   Body,
   Put,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { PostService } from './post.service';
 import { User as UserModel, Post as PostModel } from '@prisma/client';
+import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jst-auth.guard';
+import { get } from 'http';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly userService: UserService,
-    private readonly postService: PostService,
-  ) {}
 
-  @Get('post/:id')
-  async getPostById(@Param('id') id: string): Promise<PostModel> {
-    return this.postService.post({ id: Number(id) });
+  constructor(private authService: AuthService){}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.authService.login(req.user);
   }
 
-  @Get('feed')
-  async getPublishedPosts(): Promise<PostModel[]> {
-    return this.postService.posts({
-      where: { published: true },
-    });
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 
-  @Get('filtered-posts/:searchString')
-  async getFilteredPosts(
-    @Param('searchString') searchString: string,
-  ): Promise<PostModel[]> {
-    return this.postService.posts({
-      where: {
-        OR: [
-          {
-            title: { contains: searchString },
-          },
-          {
-            content: { contains: searchString },
-          },
-        ],
-      },
-    });
-  }
+  // constructor(
+  //   private readonly userService: UserService,
+  //   private readonly postService: PostService,
+  // ) {}
 
-  @Post('post')
-  async createDraft(
-    @Body() postData: { title: string; content?: string; authorEmail: string },
-  ): Promise<PostModel> {
-    const { title, content, authorEmail } = postData;
-    return this.postService.createPost({
-      title,
-      content,
-      author: {
-        connect: { email: authorEmail },
-      },
-    });
-  }
+  // @Get('post/:id')
+  // async getPostById(@Param('id') id: string): Promise<PostModel> {
+  //   return this.postService.post({ id: Number(id) });
+  // }
 
-  @Post('user')
-  async signupUser(
-    @Body() userData: { name?: string; email: string },
-  ): Promise<UserModel> {
-    return this.userService.createUser(userData);
-  }
+  // @Get('feed')
+  // async getPublishedPosts(): Promise<PostModel[]> {
+  //   return this.postService.posts({
+  //     where: { published: true },
+  //   });
+  // }
 
-  @Put('publish/:id')
-  async publishPost(@Param('id') id: string): Promise<PostModel> {
-    return this.postService.updatePost({
-      where: { id: Number(id) },
-      data: { published: true },
-    });
-  }
+  // @Get('filtered-posts/:searchString')
+  // async getFilteredPosts(
+  //   @Param('searchString') searchString: string,
+  // ): Promise<PostModel[]> {
+  //   return this.postService.posts({
+  //     where: {
+  //       OR: [
+  //         {
+  //           title: { contains: searchString },
+  //         },
+  //         {
+  //           content: { contains: searchString },
+  //         },
+  //       ],
+  //     },
+  //   });
+  // }
 
-  @Delete('post/:id')
-  async deletePost(@Param('id') id: string): Promise<PostModel> {
-    return this.postService.deletePost({ id: Number(id) });
-  }
+  // @Post('post')
+  // async createDraft(
+  //   @Body() postData: { title: string; content?: string; authorEmail: string },
+  // ): Promise<PostModel> {
+  //   const { title, content, authorEmail } = postData;
+  //   return this.postService.createPost({
+  //     title,
+  //     content,
+  //     author: {
+  //       connect: { email: authorEmail },
+  //     },
+  //   });
+  // }
+
+  // @Post('user')
+  // async signupUser(
+  //   @Body() userData: { name?: string; email: string },
+  // ): Promise<UserModel> {
+  //   return this.userService.createUser(userData);
+  // }
+
+  // @Put('publish/:id')
+  // async publishPost(@Param('id') id: string): Promise<PostModel> {
+  //   return this.postService.updatePost({
+  //     where: { id: Number(id) },
+  //     data: { published: true },
+  //   });
+  // }
+
+  // @Delete('post/:id')
+  // async deletePost(@Param('id') id: string): Promise<PostModel> {
+  //   return this.postService.deletePost({ id: Number(id) });
+  // }
 }
